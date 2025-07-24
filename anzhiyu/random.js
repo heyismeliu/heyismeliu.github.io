5a11bfd7ef28b291e607f8d9d56b0784c269775e
heyismeliu <heyismeliu@users.noreply.github.com>
2025-07-24 08:37:38 +0000

    deploy: heyismeliu/blog-source@06aa3d0c655d8033e5169cdce2724fa6b81b1efd

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/18/article-title/","2025/07/23/hello-world/"];function toRandomPost(){
+var posts=["2025/02/20/docker_install/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
