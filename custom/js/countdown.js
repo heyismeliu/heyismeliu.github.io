c25af3c7dbb0fc30b031e9625328ea807093b31c
heyismeliu <heyismeliu@users.noreply.github.com>
2025-08-11 10:33:17 +0000

    deploy: heyismeliu/blog-source@e51db2201cec9aa65bd46c65ab8b9521b5f772a2

--- custom/js/countdown.js
+++ custom/js/countdown.js
@@ -1,6 +1,6 @@
 const CountdownTimer = (() => {
     const config = {
-        targetDate: "2025-01-29",
+        targetDate: "2025-02-26",
         targetName: "春节",
         units: {
             day: { text: "今日", unit: "小时" },
